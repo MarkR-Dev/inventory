90266a5f61163d0736cf9ceab686ba5e6b81ba1e
Mark Rodgers <markrodgers.dev@gmail.com>
2025-11-17 19:28:19 +0000

    feat: add db populate script

--- db/populatedb.js
+++ db/populatedb.js
@@ -3,7 +3,47 @@
 const { Client } = require("pg");
 const { argv } = require("node:process");
 
-const SQL = ``;
+const SQL = `
+CREATE TABLE IF NOT EXISTS rarities (
+	id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  rarity_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS categories (
+	id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  category_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS items (
+	id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  item_name VARCHAR(255) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  category_id INTEGER REFERENCES categories(id) ON DELETE SET NULL,
+  rarity_id INTEGER REFERENCES rarities(id) ON DELETE SET NULL,
+  gold_cost INTEGER CHECK(gold_cost >= 0) NOT NULL,
+  quantity INTEGER CHECK(quantity >= 0) NOT NULL
+);
+
+INSERT INTO rarities (rarity_name) VALUES ('Common');
+INSERT INTO rarities (rarity_name) VALUES ('Uncommon');
+INSERT INTO rarities (rarity_name) VALUES ('Rare');
+
+INSERT INTO categories (category_name) VALUES ('Weapon');
+INSERT INTO categories (category_name) VALUES ('Armour');
+INSERT INTO categories (category_name) VALUES ('Spells');
+
+INSERT INTO items (item_name, description, category_id, rarity_id, gold_cost, quantity)
+VALUES ('Iron Sword', 'A basic iron sword', 1, 1, 10, 5);
+
+INSERT INTO items (item_name, description, category_id, rarity_id, gold_cost, quantity)
+VALUES ('Steel Sword', 'A sturdy steel sword', 1, 2, 20, 3);
+
+INSERT INTO items (item_name, description, category_id, rarity_id, gold_cost, quantity)
+VALUES ('Dusk Gloves', 'Gloves made from thick leather', 2, 1, 10, 7);
+
+INSERT INTO items (item_name, description, category_id, rarity_id, gold_cost, quantity)
+VALUES ('Scroll of Fireball', 'A fireball spell scroll', 3, 3, 100, 1);
+`;
 
 const connectionString = argv[2];
 
