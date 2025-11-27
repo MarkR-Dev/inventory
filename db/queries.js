45ba52bf9e3d95d3de70e8b9a5f4e3e6f109e307
Mark Rodgers <markrodgers.dev@gmail.com>
2025-11-27 18:19:35 +0000

    feat: add basic table styling

--- db/queries.js
+++ db/queries.js
@@ -1,7 +1,17 @@
 const pool = require("./pool");
 
+// async function getAllItems() {
+//   const { rows } = await pool.query("SELECT * FROM items;");
+//   return rows;
+// }
+
 async function getAllItems() {
-  const { rows } = await pool.query("SELECT * FROM items;");
+  const { rows } = await pool.query(
+    `SELECT items.id, item_name, description, categories.category_name, rarities.rarity_name, quantity, gold_cost FROM items 
+     LEFT JOIN categories ON items.category_id = categories.id 
+     LEFT JOIN rarities ON items.rarity_id = rarities.id
+     ORDER BY item_name;`
+  );
   return rows;
 }
 
