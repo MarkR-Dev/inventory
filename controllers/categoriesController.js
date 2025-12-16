87a7e8ddf33a834525627b99e2ce654c51753994
Mark Rodgers <markrodgers.dev@gmail.com>
2025-12-16 18:29:43 +0000

    feat: add new category form view

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -22,4 +22,46 @@ async function getSelectedCategory(req, res) {
   });
 }
 
-module.exports = { getAllCategories, getSelectedCategory };
+async function getNewCategory(req, res) {
+  res.render("newCategory", {
+    title: "Inventory | New Category",
+    prevData: { category_name: "" },
+  });
+}
+
+const validateCategory = [
+  body("category_name")
+    .notEmpty()
+    .withMessage("Category name cannot be empty")
+    .isLength({ min: 1, max: 255 })
+    .withMessage("Category name length must be between 1-255 characters"),
+];
+
+const postNewCategory = [
+  validateCategory,
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      const prevData = matchedData(req, { onlyValidData: false });
+
+      return res.status(400).render("newCategory", {
+        title: "Inventory | New Category",
+        errors: errors.array(),
+        prevData: prevData,
+      });
+    }
+
+    // todo: db query to add new category
+    const newCategory = matchedData(req);
+
+    res.send("post /new cat");
+  },
+];
+
+module.exports = {
+  getAllCategories,
+  getSelectedCategory,
+  getNewCategory,
+  postNewCategory,
+};
--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -2,11 +2,16 @@ const { Router } = require("express");
 const {
   getAllCategories,
   getSelectedCategory,
+  getNewCategory,
+  postNewCategory,
 } = require("../controllers/categoriesController");
 
 const categoriesRouter = Router();
 
 categoriesRouter.get("/", getAllCategories);
+
+categoriesRouter.get("/new", getNewCategory);
+categoriesRouter.post("/new", postNewCategory);
 
 categoriesRouter.get("/:id", getSelectedCategory);
 
